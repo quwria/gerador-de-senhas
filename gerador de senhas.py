e8298fa11e8d9c5c8157f164a229fe53cf5701e3
Alexandre <125616314+quwria@users.noreply.github.com>
2023-03-18 15:06:01 -0300

    Update gerador de senhas.py

--- gerador de senhas.py
+++ gerador de senhas.py
@@ -1,7 +1,11 @@
 import string
 import random
-abc = string.ascii_letters
-simbs = string.punctuation
-abcssimbs = abc + simbs
-print(
-   ''.join(random.SystemRandom().choices(abcssimbs, k=8)))
+
+def gerar_senha():
+    abc = string.ascii_letters + string.digits
+    simbs = ''.join(random.sample('!@#$%&', 2))
+    senha = ''.join(random.choice(abc) for i in range(10)) + simbs
+    senha = ''.join(random.sample(senha, len(senha)))
+    return senha
+
+print(gerar_senha())
